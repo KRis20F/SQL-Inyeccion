94a520ecf87c88cca6ee573046021db4ee2904af
KRis20f <cristiangutierrez008@gmail.com>
2024-04-26 20:11:35 +0200

    Entrega de trabajo de SQL inyecciom

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ Created on February 2023
 
 @author: Albert ETPX
 """
-
 # Importación de módulos externos
 import mysql.connector
 from flask import Flask,render_template,request;
@@ -16,7 +15,7 @@ def connectBD():
     db = mysql.connector.connect(
         host = "localhost",
         user = "root",
-        passwd = "claumestra",
+        passwd = "",
         database = "users"
     )
     return db
@@ -55,11 +54,10 @@ def initBD():
 def checkUser(user,password):
     bd=connectBD()
     cursor=bd.cursor()
-
-    query=f"SELECT user,name,surname1,surname2,age,genre FROM users WHERE user='{user}'\
-            AND password='{password}'"
+    query="SELECT user,name,surname1,surname2,age,genre FROM users WHERE user= %s AND password = %s" #He creado esta parte
+    values = (user, password)
     print(query)
-    cursor.execute(query)
+    cursor.execute(query, values)
     userData = cursor.fetchall()
     bd.close()
     
@@ -67,11 +65,18 @@ def checkUser(user,password):
         return False
     else:
         return userData[0]
-
-# cresteUser: crea un nuevo usuario en la BD
-def createUser(user,password,name,surname1,surname2,age,genre):
     
-    return
+# cresteUser: crea un nuevo usuario en la BD
+def createUser(usuario,newPasswd,newNom,newCognom,newCognom2,newEdad,newGenero):
+    bd=connectBD()
+    cursor = bd.cursor()
+    query = "INSERT INTO users (user, password, name, surname1, surname2, age, genre) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    values = usuario, newPasswd,newNom,newCognom,newCognom2,newEdad,newGenero
+    cursor.execute(query, values)
+    bd.commit()
+    bd.close()
+    
+# CREACION DE NUEVO USUARIO POR MI
 
 # Secuencia principal: configuración de la aplicación web ##########################################
 # Instanciación de la aplicación web Flask
@@ -87,10 +92,29 @@ def login():
     initBD()
     return render_template("login.html")
 
-@app.route("/signin")
+@app.route("/signin.html")
 def signin():
-    return "SIGN IN PAGE"
+    initBD()
+    return render_template("signin.html")
 
+# CODIGO HECHO
+@app.route("/registre", methods = ('GET', 'POST'))
+def registre():
+    if request.method == ('POST'):
+        formData = request.form
+        newuser = formData['usuario']
+        newPasswd = formData['contraseña']
+        newNom = formData['nombre']
+        newCognom = formData['apellido']
+        newCognom2 = formData['apellido2']
+        newEdad = formData['edad']
+        newGenere = formData['genero'] 
+        createUser(newuser,newPasswd,newNom,newCognom,newCognom2,newEdad,newGenere)
+        return render_template("home.html")  
+    else:
+        return "NADA"
+     
+#FIN DEL CODIGO HECHO PARA EL FORMULARIO DEL REGISTRO 
 @app.route("/results",methods=('GET', 'POST'))
 def results():
     if request.method == ('POST'):
